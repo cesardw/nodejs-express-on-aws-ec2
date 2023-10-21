805e9e89dd7b6f34b7e1a0ed1fea7ccd259bd22a
Cesar Delgado <cesards91@gmail.com>
2023-10-20 19:39:56 -0600

    cambio realizado en texto

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>DevOps en Proceso</h1> <h4>Prueba CI/CD</h4> <p>Version 1.0</p>');
+  res.send('<h1>DevOps en Proceso</h1> <h4>Prueba CI/CD desde Github</h4> <p>Version 1.0</p>');
 })
+
 
 app.get('/products', (req, res) => {
   res.send([
